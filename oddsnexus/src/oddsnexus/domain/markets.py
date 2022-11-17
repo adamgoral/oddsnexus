045953fabb83f809ac381ac2c589667435e80b38
Adam Goral <3025549+adamgoral@users.noreply.github.com>
2022-11-17 20:35:34 +0000

    added markets module and test

--- oddsnexus/src/oddsnexus/domain/markets.py
+++ oddsnexus/src/oddsnexus/domain/markets.py
@@ -1,5 +1,21 @@
+from typing import Dict, List
+
 import faust
 
+Price = int
+Volume = int
+ID = str
+EventStatus = str
 
-class Market(faust.Model):
-    id: str
+class Outcome(faust.Record):
+    id: ID
+
+class Event(faust.Record):
+    event_type: str
+    outcomes: List[Outcome]
+    status: EventStatus 
+
+
+class MarketBook(faust.Record):
+    bids: Dict[Price, Volume]
+    offers: Dict[Price, Volume]

962d0c39398c1d90ce1211c85e7f7566f338b3fb
Adam Goral <3025549+adamgoral@users.noreply.github.com>
2022-11-18 19:28:55 +0000

    markets update

--- oddsnexus/src/oddsnexus/domain/markets.py
+++ oddsnexus/src/oddsnexus/domain/markets.py
@@ -1,3 +1,4 @@
+import datetime as dt
 from typing import Dict, List
 
 import faust
@@ -5,17 +6,52 @@ import faust
 Price = int
 Volume = int
 ID = str
+DateTime = dt.datetime
 EventStatus = str
+Team = str
+SportsEventStatus = str
+RaceTrack = str
+Runner = str
+Goals = int
 
-class Outcome(faust.Record):
+class SportsEventOutcome(faust.Record):
+    ...
+
+class SoccerScores(faust.Record):
+    home: Goals
+    away: Goals
+
+class SoccerMatchOutcome(SportsEventOutcome):
+    first_half: SoccerScores
+    second_half: SoccerScores
+
+class HorseRaceOutcome(SportsEventOutcome):
+    finishers: List[Runner]
+
+class SportsEvent(faust.Record):
     id: ID
+    start: DateTime
+    status: SportsEventStatus
+    outcome: SportsEventOutcome
 
 class Event(faust.Record):
-    event_type: str
-    outcomes: List[Outcome]
+    id: ID
     status: EventStatus 
 
+class SoccerMatch(SportsEvent):
+    home: Team
+    away: Team
+    outcome: SoccerMatchOutcome
 
-class MarketBook(faust.Record):
+class HorseRace(SportsEvent):
+    track: RaceTrack
+    runners: List[Runner]
+    outcome: HorseRaceOutcome
+
+class HorseRaceWinner(Event):
+    runner: Runner
+
+class OrderBook(faust.Record):
+    event_id: ID
     bids: Dict[Price, Volume]
     offers: Dict[Price, Volume]

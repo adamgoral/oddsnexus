8d777b0b801ce96c8869518dc66c4b9595829d61
Adam Goral <3025549+adamgoral@users.noreply.github.com>
2023-08-21 18:04:51 +0100

    Added sporting life horse races asset to dagster

--- etl/oddsnexus-etl-dagster/oddsnexus_etl_dagster/assets.py
+++ etl/oddsnexus-etl-dagster/oddsnexus_etl_dagster/assets.py
@@ -1,1 +1,15 @@
+from datetime import datetime
+from dagster import DailyPartitionsDefinition, asset, OpExecutionContext
 
+from oddsnexus.repositories.racing import SportingLifeSource
+
+@asset(partitions_def=DailyPartitionsDefinition(start_date="2022-01-01"))
+def sporting_life_horse_races(context: OpExecutionContext):
+    source = SportingLifeSource('https://www.sportinglife.com/api')
+    date_str = context.asset_partition_key_for_output()
+    date = datetime.strptime(date_str, "%Y-%m-%d").date()
+    races = source.list_races(date)
+    context.add_output_metadata({
+        'races_count': len(races)
+    })
+    return races

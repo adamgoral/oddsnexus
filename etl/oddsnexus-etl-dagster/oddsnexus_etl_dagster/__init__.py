40a8cf2cd7c9120adafe8992838c462d8742792a
Adam Goral <3025549+adamgoral@users.noreply.github.com>
2023-08-22 19:42:47 +0100

    corrected scheduler and job definitions

--- etl/oddsnexus-etl-dagster/oddsnexus_etl_dagster/__init__.py
+++ etl/oddsnexus-etl-dagster/oddsnexus_etl_dagster/__init__.py
@@ -1,9 +1,26 @@
-from dagster import Definitions, load_assets_from_modules
+from dagster import DailyPartitionsDefinition, Definitions, FilesystemIOManager, build_schedule_from_partitioned_job, define_asset_job, load_assets_from_modules
 
 from . import assets
 
 all_assets = load_assets_from_modules([assets])
 
+
+partitioned_asset_job = define_asset_job("partitioned_job",
+                                         selection=all_assets,
+                                         partitions_def=DailyPartitionsDefinition(start_date="2022-01-01"))
+
+
+asset_partitioned_schedule = build_schedule_from_partitioned_job(
+    partitioned_asset_job,
+    hour_of_day=2
+)
+
+
 defs = Definitions(
     assets=all_assets,
+    resources={
+        'io_manager': FilesystemIOManager(base_dir='./data')
+    },
+    schedules=[asset_partitioned_schedule],
+    jobs=[partitioned_asset_job]
 )
--- etl/oddsnexus-etl-dagster/oddsnexus_etl_dagster/assets.py
+++ etl/oddsnexus-etl-dagster/oddsnexus_etl_dagster/assets.py
@@ -3,7 +3,7 @@ from dagster import AssetSelection, DailyPartitionsDefinition, ScheduleDefinitio
 
 from oddsnexus.repositories.racing import SportingLifeSource
 
-@asset(partitions_def=DailyPartitionsDefinition(start_date="2022-01-01"))
+@asset(group_name='sporting_life')
 def sporting_life_horse_races(context: OpExecutionContext):
     source = SportingLifeSource('https://www.sportinglife.com/api')
     date_str = context.asset_partition_key_for_output()
@@ -13,21 +13,3 @@ def sporting_life_horse_races(context: OpExecutionContext):
         'races_count': len(races)
     })
     return races
-
-
-partitioned_asset_job = define_asset_job("partitioned_job", selection=[sporting_life_horse_races], partitions_def=DailyPartitionsDefinition(start_date="2022-01-01"))
-
-
-asset_partitioned_schedule = build_schedule_from_partitioned_job(
-    partitioned_asset_job
-)
-
-
-defs = Definitions(
-    assets=[sporting_life_horse_races],
-    resources={
-        'io_manager': FilesystemIOManager(base_dir='./data')
-    },
-    schedules=[asset_partitioned_schedule],
-    jobs=[partitioned_asset_job]
-)

fcb53bc50cf7861b41cf6bebc35c04731931c38d
Adam Goral <3025549+adamgoral@users.noreply.github.com>
2023-08-22 20:02:53 +0100

    fixed asset partition

--- etl/oddsnexus-etl-dagster/oddsnexus_etl_dagster/assets.py
+++ etl/oddsnexus-etl-dagster/oddsnexus_etl_dagster/assets.py
@@ -3,13 +3,14 @@ from dagster import AssetSelection, DailyPartitionsDefinition, ScheduleDefinitio
 
 from oddsnexus.repositories.racing import SportingLifeSource
 
-@asset(group_name='sporting_life')
+@asset(group_name='sporting_life', partitions_def=DailyPartitionsDefinition(start_date="2022-01-01"))
 def sporting_life_horse_races(context: OpExecutionContext):
     source = SportingLifeSource('https://www.sportinglife.com/api')
     date_str = context.asset_partition_key_for_output()
     date = datetime.strptime(date_str, "%Y-%m-%d").date()
     races = source.list_races(date)
     context.add_output_metadata({
+        'date': date,
         'races_count': len(races)
     })
     return races
